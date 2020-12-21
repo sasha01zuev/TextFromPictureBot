ec5c4ebb1c08d1dd4af9e142bcefa33ecc0abe0b
sasha_zuev <sashazuev01@gmail.com>
2020-12-21 12:05:06 +0200

    Beta extracting text from picture

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,5 +1,4 @@
 from .start import dp
 from .echo import dp
 from .getting_picture import dp
-from .getting_text_from_pic import dp
 __all__ = ["dp"]
--- handlers/users/getting_picture.py
+++ handlers/users/getting_picture.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import ContentType, Message
 import cv2
 import pytesseract
+from PIL import Image
 from loader import dp
 
 
@@ -12,5 +13,8 @@ async def getting_photo(message: Message):
 
     await message.answer("Скачал фото! 🤨😲")
     picture_id = message.photo[-1].file_id
-    await message.photo[-1].download('pictures/picture.jpg')
+    await message.photo[-1].download('pictures/picture.png')
     print(picture_id)
+    print(pytesseract.get_languages(config=''))
+    config = r'--oem 3 --psm 3'
+    print(pytesseract.image_to_string('pictures/picture.зтп', lang='rus', config=config))
