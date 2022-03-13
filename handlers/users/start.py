53e775d6c7c3b7cdab2bb3afb191cc15ee398c6b
Alex <41638681+sasha01zuev@users.noreply.github.com>
2022-03-13 20:42:28 +0200

    Fixed start command.

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -12,11 +12,14 @@ async def start(message: Message):
     user_id = message.from_user.id
     username = message.from_user.username
     first_name = message.from_user.first_name
+    user = await db.get_user(user_id)
 
-    # TODO: correct start - checking user existence
-    await db.add_user(user_id, username, first_name)
-    await db.add_user_info(user_id)
-    await message.answer(_("Send me picture with text and I send you only text💬\n"
-                         "I'll try to make it right😅"))
+    if not user:
+        await db.add_user(user_id, username, first_name)
+        await db.add_user_info(user_id)
+        await message.answer(_("Send me picture with text and I send you only text💬\n"
+                             "I'll try to make it right😅"))
 
-    await message.answer(_('Choose your language:'), reply_markup=language_keyboard)
+        await message.answer(_('Choose your language:'), reply_markup=language_keyboard)
+    else:
+        await message.answer(_('You have already been registered!'))
