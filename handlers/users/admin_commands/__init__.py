41a2a439bb25d61f5293986d616205a7ffcdbcc9
Alex <41638681+sasha01zuev@users.noreply.github.com>
2022-03-17 14:54:58 +0200

    Implemented: admin_command_list

--- handlers/users/admin_commands/__init__.py
+++ handlers/users/admin_commands/__init__.py
@@ -2,3 +2,5 @@ from .add_to_blacklist import dp
 from .remove_from_blacklist import dp
 from .mass_mailing import dp
 from .message_to_user import dp
+from .admin_command_list import dp
+
---
+++ handlers/users/admin_commands/admin_command_list.py
@@ -0,0 +1,35 @@
+from aiogram import types
+from aiogram.dispatcher.filters import Command
+
+from data.config import ADMINS_ID, MAIN_ADMIN
+from loader import dp, db
+from loguru import logger
+import datetime
+
+
+@dp.message_handler(Command('admin_command_list'), user_id=ADMINS_ID)
+async def admin_command_list(message: types.Message):
+    user_id = message.from_user.id
+
+    if int(user_id) == int(MAIN_ADMIN):
+        await message.answer(
+            '/mass_mailing - Рассылка всем пользователям\n'
+            'Template: /mass_mailing (Audience: [ru/en/uk/all]) (Photo: [true/false]) '
+            '(First_button(Optional): [Button_name-link]) (Second_button(Optional): '
+            '[Button_name-link]) (Third_button(Optional): [Button_name-link])\n\n'
+            '/autb - Добавить пользователя в черный список (Add User To Blacklist\n'
+            'Template: /autb [user_id]\n\n'
+            '/rufb - Удалить пользователя с чёрного списка (Remove User From Blacklist)\n'
+            'Template: /rufb [user_id]\n\n'
+            '/mtu - Отправить сообщение пользователю (Message To User)\n'
+            'Template: /mtu [user_id]'
+        )
+    else:
+        await message.answer(
+            '/autb - Добавить пользователя в черный список (Add User To Blacklist\n'
+            'Template: /autb [user_id]\n\n'
+            '/rufb - Удалить пользователя с чёрного списка (Remove User From Blacklist)\n'
+            'Template: /rufb [user_id]\n\n'
+            '/mtu - Отправить сообщение пользователю (Message To User)\n'
+            'Template: /mtu [user_id]'
+        )
